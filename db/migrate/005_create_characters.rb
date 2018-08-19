40860350411b1bb06d194d760bb0adea3d7983e3
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-19 18:38:47 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
       t.string :name 
       t.integer :show_id
       t.integer :actor_id 
+      #Had to get help for this part. This is much easier than the procedural way of doing it, such as adding this character to the show in the models folder
       t.belongs_to :actor, index: true
       t.belongs_to :show, index: true
     end
