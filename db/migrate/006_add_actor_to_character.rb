874da0100c0ebd2c64b044a896b7aaca7511c541
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-19 18:25:51 +0000

    automatically backed up by learn

--- db/migrate/006_add_actor_to_character.rb
+++ db/migrate/006_add_actor_to_character.rb
@@ -1,4 +1,4 @@
-class AddActorToCharacters < ActiveRecord::Migration[4.2]  
+class AddActorToCharacter < ActiveRecord::Migration[4.2]  
   def change
     add_column :characters, :actor_id, :integer 
   end  
