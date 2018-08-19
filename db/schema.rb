7f8a07cd3d82d8c196cedea8ac85ef1acf853659
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-19 18:09:21 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
